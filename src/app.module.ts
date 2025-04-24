f5b0372aa48854bf0d9e0c262368cb4d5b52b58c
MikelBarajas38 <mikelbarajas388@gmail.com>
2025-04-24 01:02:53 -0600

    oops

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PaymentsController } from './payments/payments.controller';
+import { PaymentsService } from './payments/payments.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule
+  ],
+  controllers: [AppController, PaymentsController],
+  providers: [AppService, PaymentsService],
 })
 export class AppModule {}
